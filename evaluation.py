c29a8b1b88cef43f14ab6eb9e493a167973b55a4
blipp <mail@benjaminlipp.de>
2014-11-18 18:23:10 +0100

    Ready for use under supervision
    The code needs to be tidied up and documented

--- evaluation.py
+++ evaluation.py
@@ -10,15 +10,20 @@ import os
 import glob
 import json
 import urllib.request
+import csv
 from datetime import datetime, time
 from pprint import pprint
+
+from simpleodspy.sodsspreadsheet import SodsSpreadSheet
+from simpleodspy.sodsods import SodsOds
 
 ###
 # Configuration options
 ###
 
-# Lehrveranstaltungsnummern of courses to get from KitHub
-lvnrs = [4010031, 4010111, 4010011]
+input_file = "input.csv"
+output_file = "timetable.ods"
+output_comparison_file = "output.ods"
 
 # translation of weekdays
 # TODO: change to strftime
@@ -51,17 +56,38 @@ timetable_blocks = [{'time': time(hour=8, minute=0), 'block': 1},
                     {'time': time(hour=15, minute=45), 'block': 5},
                     {'time': time(hour=17, minute=30), 'block': 6}]
 
+font_family = "Liberation Sans"
+font_size_name = "12pt"
+font_size = "10pt"
+
+block_separator_border = "2pt solid #000000"
+block_separator_appointments = "1pt solid #000000"
+
+# set to true if you want the timetable printed to stdout
+terminal_timetable = False
+
 ###
 # End of configuration options
 ###
 
+# rows, columns
+# TODO: change everything to row, column
+spreadsheet_timetable = SodsSpreadSheet(200, 20)
+
+output_comparison_col_exclude = 4
+output_comparison_col_uebung = 5
+output_comparison_col_praktikum = 6
+output_comparison_col_found_appointment = 7
+output_comparison_col_name_vvz = 8
 
 class Course:
 
     def __init__(self, id, lvnr, name, lecturer):
-        self.id = id
-        self.lvnr = lvnr
+        self.id = int(id)
+        self.lvnr = int(lvnr)
         self.name = name
+        self.name_short = ""
+        self.category = ""
         self.lecturer = lecturer
         # all occurrences over the whole semester
         self.appointments = []
@@ -97,39 +123,42 @@ def get_courses(course_numbers):
 
 
 # download data in json format of one course to a file
-def get_course(course_number):
+def get_course(course_lvnr):
+    if os.path.isfile("{0}/{1}.json".format(json_directory, course_lvnr)):
+        verbose("course {0} already downloaded.".format(course_lvnr))
+        return
     try:
-        verbose("fetch course '{0}'.".format(course_number))
+        verbose("fetch course '{0}'.".format(course_lvnr))
 
         # use rest api to access data
         response = urllib.request.urlopen(
-            "http://www.kithub.de/api/terms/{0}/events.json?type=detail&no={1}".format(term_id, course_number)).readall().decode('utf8')
+            "http://www.kithub.de/api/terms/{0}/events.json?type=detail&no={1}".format(term_id, course_lvnr)).readall().decode('utf8')
         # TODO: catch exceptions for next line
         data = json.loads(response)
 
         if not os.path.exists(json_directory):
             os.makedirs(json_directory)
-        with open("{0}/{1}.json".format(json_directory, course_number), 'w') as file:
+        with open("{0}/{1}.json".format(json_directory, course_lvnr), 'w') as file:
             file.write(json.dumps(data, indent=2, sort_keys=True))
 
     except urllib.error.URLError as err:
         print("URLError while fetching course '{0}': {1}".format(
-            course_number, err.reason))
+            course_lvnr, err.reason))
     except urllib.error.HTTPError as err:
         print("HTTPError '{0}' while fetching course '{1}': {2}".format(
-            err.code, course_number, err.reason))
+            err.code, course_lvnr, err.reason))
     except ValueError:
         print("ValueError while parsing content as JSON for course '{0}'.".format(
-            course_number))
+            course_lvnr))
     except:
         print(
-            "Unexpected error while fetching course '{0}': ".format(course_number))
+            "Unexpected error while fetching course '{0}': ".format(course_lvnr))
         print(sys.exc_info()[0])
 
 
 def build_timetable():
     # iterate over all courses
-    for course_id, course in courses.items():
+    for course_lvnr, course in courses.items():
         found_appointment = False
         # iterate over all events
         for appointment in course.appointments:
@@ -141,32 +170,35 @@ def build_timetable():
                 block = appointment.get_timetable_block()
 
                 # append the date to the list of dates in the timetable
-                if course_id not in timetable[weekday][block]:
-                    timetable[weekday][block][course_id] = []
-                timetable[weekday][block][course_id].append(appointment)
+                if course_lvnr not in timetable[weekday][block]:
+                    timetable[weekday][block][course_lvnr] = []
+                timetable[weekday][block][course_lvnr].append(appointment)
 
                 # append this [weekday, block] to the list of occurrences
                 if [weekday, block] not in course.occurrences:
                     course.occurrences.append([weekday, block])
 
         if not found_appointment:
-            courses_missed[course.id] = course
-            print(
-                "ERROR: Did not find any appointment within the evaluation period for the course '{0}', LV-Nr. {1}!".format(course.name, course.lvnr))
+            courses_missed[course.lvnr] = course
+
+
+def load_course_from_filesystem(filename):
+    with open(filename, 'r') as f:
+        data = json.load(f)
+    if len(data) > 1:
+        print(
+            "There is more than 1 object encoded in file {0}. Please have a look. For now, I import them all.".format(filename))
+    for i, object in enumerate(data):
+        course = Course(
+            object['id'], object['no'], object['name'], object['lecturer'])
+        course.appointments = course_parse_appointments(object['dates'])
+    return course
 
 
 def load_courses_from_filesystem():
-    for file in glob.glob("{0}/*.json".format(json_directory)):
-        with open(file, 'r') as f:
-            data = json.load(f)
-        if len(data) > 1:
-            print(
-                "There is more than 1 object encoded in file {0}. Please have a look. For now, I import them all.".format(file))
-        for i, object in enumerate(data):
-            course = Course(
-                object['id'], object['no'], object['name'], object['lecturer'])
-            course.appointments = course_parse_appointments(object['dates'])
-            courses[course.id] = course
+    for filename in glob.glob("{0}/*.json".format(json_directory)):
+        course = load_course_from_filesystem(filename)
+        courses[course.lvnr] = course
 
 
 # replace datetime in string format by an actual datetime object
@@ -192,15 +224,63 @@ def verbose(str):
     print(str)
 
 
+def print_timetable(str):
+    if terminal_timetable:
+        print(str)
+
+def column_name_from_number(column_number):
+    return spreadsheet_timetable.encodeColName(column_number)
+
+
+def cell_coordinate(row_number, column_number):
+    return spreadsheet_timetable.encodeCellName(row_number, column_number)
+
+
+# content must be string
+def print_cell(spreadsheet, row_number, column_number, content, font_size=font_size, font_family=font_family):
+    spreadsheet.setValue(cell_coordinate(row_number, column_number), content)
+    spreadsheet.setStyle(cell_coordinate(row_number, column_number), font_family = font_family)
+    spreadsheet.setStyle(cell_coordinate(row_number, column_number), font_size = font_size)
+
+
+def print_row(spreadsheet, row_number, row):
+    for i, cell in enumerate(row):
+        print_cell(spreadsheet, row_number, i+1, cell)
+
+
+# TODO: only overwrite if option --overwrite is passed or ask
+if os.path.exists(output_file):
+    os.remove(output_file)
+
+# holds all courses as Course objects with course.lvnr as key
+courses = {}
+# holds courses for which no appointment was found within the time interval
+courses_missed = {}
+
 eva_starttime = parse_datetime(eva_starttime_str)
 eva_endtime = parse_datetime(eva_endtime_str)
 
 print("Printing time interval {0} to {1}".format(eva_starttime, eva_endtime))
 
-# holds all courses as Course objects with course.id as key
-courses = {}
-# holds courses for which no appointment was found within the time interval
-courses_missed = {}
+with open(input_file, newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    row_counter = 0
+    for row in spamreader:
+        if row_counter > 0 and row[output_comparison_col_exclude - 1] != "x":
+            get_course(row[0])
+            course = load_course_from_filesystem("{0}/{1}.json".format(json_directory, int(row[0])))
+            course.category = row[1]
+            course.name_short = row[2]
+            if row[output_comparison_col_uebung - 1] == "x":
+                course.name_short += " +Übung"
+            if row[output_comparison_col_praktikum - 1] == "x":
+                course.name_short += " +Prakt"
+            if row[1] != "":
+                course.name_short += " ({0})".format(row[1])
+            courses[course.lvnr] = course
+        row_counter += 1
+
+spreadsheet_output_comparison = SodsSpreadSheet(row_counter, 10)
 
 # holds all days, and within them all blocks and within these the courses
 timetable = {}
@@ -211,36 +291,146 @@ for day in range(1, 6):
         blocks[block] = {}
     timetable[day] = blocks
 
-# download data from KitHub, this saves them to disk
-get_courses(lvnrs)
-
-# load the files
-load_courses_from_filesystem()
-
 # assemble the timetable
 build_timetable()
 
+# spreadsheet columns and rows start at 1, plus leave the first empty
+column_start = 2
+row_start = 2
+
+# number of columns and rows per entry
+appointment_width = 2
+appointment_height = 3
+
+# get maximum number of lectures that has to be fit into each block
+# but set it to 1 at least
+block_height = {}
+for block in range(1, 9):
+    block_height[block] = 1
+
+for block in range(1, 9):
+    for weekday in range(1, 6):
+        new_height = len(timetable[weekday][block])
+        if new_height > block_height[block]:
+            block_height[block] = new_height
+
+# print first column: block numbers
+current_row = row_start + 1
+for block in range(1, 9):
+    print_cell(spreadsheet_timetable, current_row, column_start, str(block))
+    current_row += block_height[block] * appointment_height
+
 # print timetable
 for weekday in range(1, 6):
-    print("{0}".format(weekdays[weekday]))
+    print_timetable("{0}".format(weekdays[weekday]))
+    weekday_column = column_start + 1 + (weekday-1) * appointment_width
+    block_row_start = row_start + 1
+    print_cell(spreadsheet_timetable, row_start, weekday_column, weekdays[weekday], font_size_name)
     for block in range(1, 9):
-        print("  {0}. Block".format(block))
-        for course_id in timetable[weekday][block].keys():
-            course = courses[course_id]
-            print("    {0}, {1}".format(course.name, course.lecturer))
-            for appointment in timetable[weekday][block][course_id]:
-                print("      {0}, {1}".format(appointment.start.strftime("%d.%m."), appointment.room))
+        print_timetable("  {0}. Block".format(block))
+        for i, course_lvnr in enumerate(timetable[weekday][block].keys()):
+            course = courses[course_lvnr]
+            print_timetable("    {0}, {1}".format(course.name, course.lecturer))
+            course_row_start = block_row_start + i*appointment_height
+            print_cell(spreadsheet_timetable, course_row_start, weekday_column, course.name_short, font_size_name)
+            print_cell(spreadsheet_timetable, course_row_start + 1, weekday_column, course.lecturer)
+            course_room = ""
+            course_dates = ""
+            for appointment in timetable[weekday][block][course_lvnr]:
+                print_timetable("      {0}, {1}".format(appointment.start.strftime("%d.%m."), appointment.room))
+                if course_dates != "":
+                    course_dates += ", "
+                course_dates += appointment.start.strftime("%d.%m.")
+                if course_room != str(appointment.room):
+                    if course_room != "":
+                        course_room += ", "
+                    course_room += str(appointment.room)
+            course_other_dates = ""
             for weekday_other, block_other in course.occurrences:
                 if not (weekday_other == weekday and block_other == block):
-                    print(
+                    print_timetable(
                         "      Auch {0}/{1}".format(weekdays_short[weekday_other], block_other))
-                    for appointment_other in timetable[weekday_other][block_other][course_id]:
-                        print(
+                    if course_other_dates != "":
+                        course_other_dates += ", "
+                    course_other_dates += "{0}/{1}".format(weekdays_short[weekday_other], block_other)
+                    for appointment_other in timetable[weekday_other][block_other][course_lvnr]:
+                        print_timetable(
                             "        {0}".format(appointment_other.start.strftime("%d.%m.")))
+            if course_other_dates != "":
+                course_dates += "; auch " + course_other_dates
+            print_cell(spreadsheet_timetable, course_row_start, weekday_column + 1, course_room)
+            print_cell(spreadsheet_timetable, course_row_start + 2, weekday_column, course_dates)
+        block_row_start += block_height[block] * appointment_height
 
-print("Für die folgenden Veranstaltungen wurde kein Termin im gewählten Zeitfenster gefunden:")
+
+# set borders on timetable
+
+# first the horizontal ones
+block_row_start = row_start + 1
+timetable_last_row = sum(block_height.values()) * appointment_height + block_row_start
+for row in range(block_row_start, timetable_last_row, 3):
+    spreadsheet_timetable.setStyle("{0}:{1}".format(cell_coordinate(row, column_start+1), cell_coordinate(row, 5*appointment_width + column_start)), border_top=block_separator_appointments)
+
+# print first column: block numbers
+current_row = row_start + 1
+for block in range(1, 9):
+    spreadsheet_timetable.setStyle("{0}:{1}".format(cell_coordinate(current_row, column_start+1), cell_coordinate(current_row, 5*appointment_width + column_start)), border_top=block_separator_border)
+    current_row += block_height[block] * appointment_height
+
+# now the vertical ones
+# no border right of Friday
+for weekday in range(1, 5):
+    weekday_column = column_start + 1 + (weekday-1) * appointment_width + 1
+    spreadsheet_timetable.setStyle("{0}:{1}".format(cell_coordinate(row_start, weekday_column), cell_coordinate(timetable_last_row - 1, weekday_column)), border_right=block_separator_border)
+
+print("Für die folgenden {0} Veranstaltungen wurde kein Termin im gewählten Zeitfenster gefunden:".format(len(courses_missed.values())))
 if not courses_missed:
     print("  keine")
 else:
     for course in courses_missed.values():
         print("{0}, {1}, {2}".format(course.lvnr, course.name, course.lecturer))
+
+
+# produce ODS file for timetable
+Sods_ods = SodsOds(spreadsheet_timetable)
+Sods_ods.save(output_file)
+
+# produce ODS file for comparison of input and output
+# TODO: if file exists, only overwrite if option --overwrite is passed (or ask)
+with open(input_file, newline='') as csvfile_input:
+    spamreader = csv.reader(csvfile_input, delimiter=',', quotechar='"')
+
+    first_line = True
+    for i, row in enumerate(spamreader):
+        if first_line:
+            # copy the table header
+            print_row(spreadsheet_output_comparison, i+1, row)
+            spreadsheet_output_comparison.setValue(cell_coordinate(i+1, output_comparison_col_name_vvz), "Name aus VVZ")
+            spreadsheet_output_comparison.setValue(cell_coordinate(i+1, output_comparison_col_found_appointment), "Termin gefunden?")
+            first_line = False
+        else:
+            print_row(spreadsheet_output_comparison, i+1, row)
+            if int(row[0]) not in courses_missed and int(row[0]) not in courses:
+                found_appointment = "…"
+            elif int(row[0]) not in courses_missed:
+                found_appointment = "ja"
+            else:
+                found_appointment = "nein"
+            if found_appointment != "…":
+                spreadsheet_output_comparison.setValue(cell_coordinate(i+1, output_comparison_col_name_vvz), courses[int(row[0])].name)
+                spreadsheet_output_comparison.setValue(cell_coordinate(i+1, output_comparison_col_found_appointment), found_appointment)
+                # color the cell if the name is different as this could be an issue
+                if row[2] != courses[int(row[0])].name:
+                    spreadsheet_output_comparison.setStyle(cell_coordinate(i+1, output_comparison_col_name_vvz), background_color= "#ff0000")
+            # color the cell if no appointment was found in the given time interval
+            if found_appointment == "nein":
+                spreadsheet_output_comparison.setStyle(cell_coordinate(i+1, output_comparison_col_found_appointment), background_color= "#ff0000")
+            elif found_appointment == "…":
+                spreadsheet_output_comparison.setStyle(cell_coordinate(i+1, output_comparison_col_found_appointment), background_color= "#ffffe0")
+
+# TODO: only overwrite if option --overwrite is passed or ask
+if os.path.exists(output_comparison_file):
+    os.remove(output_comparison_file)
+
+Sods_ods = SodsOds(spreadsheet_output_comparison)
+Sods_ods.save(output_comparison_file)
